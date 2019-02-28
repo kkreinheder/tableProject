083cc663ba7c14c0ba417613a9134d96911b9eae
Kyle Kreinheder <kk566@njit.edu>
2019-02-28 08:42:46 -0500

    Added example files and checked for errors

---
+++ tests/FileTest.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class FileTest extends TestCase
+{
+
+    public function testDataDirectory() {
+        $this->assertDirectoryExists('data');
+    }
+    public function testCSVFileExists() {
+        $this->assertFileExists('data/data.csv');
+    }
+
+    public function testFileObject() {
+        $this->assertInstanceOf(File::class, new File());
+    }
+
+    public function testReadCSVtoArrayExists() {
+
+        $this->assertTrue(
+            method_exists(File::class, 'readCSVtoArray')
+        );
+    }
+
+    public function testReadCSVtoArray() {
+
+        $records = File::readCSVtoArray("data/data.csv",'Car');
+        print_r($records);
+
+    }
+}
